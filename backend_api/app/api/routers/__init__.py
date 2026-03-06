1803d6f65581252376bb5a9f4a99413687d7619c
cl302 <cl686@uowmail.edu.au>
2026-03-06 15:20:15 +1100

    Update __init__.py

--- backend_api/app/api/routers/__init__.py
+++ backend_api/app/api/routers/__init__.py
@@ -1,5 +1,9 @@
 from app.api.routers.health import router as health_router
+from app.api.routers.residents import router as residents_router
+from app.api.routers.bookings import router as bookings_router
 
 all_routers = [
     health_router,
+    residents_router,
+    bookings_router,
 ]
