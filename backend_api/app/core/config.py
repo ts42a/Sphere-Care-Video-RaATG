f14f207398c1577464dae328c101618fb39a3aea
cl302 <cl686@uowmail.edu.au>
2026-03-06 15:42:53 +1100

    Update config.py

--- backend_api/app/core/config.py
+++ backend_api/app/core/config.py
@@ -3,6 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:123@localhost:5432/spherecare")
-ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "*")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:123@localhost:5432/spherecare"
+)
+
+ALLOWED_ORIGINS = os.getenv(
+    "ALLOWED_ORIGINS",
+    "*"
+)
 
