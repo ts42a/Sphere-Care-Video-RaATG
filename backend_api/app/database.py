1857c8730cbc95d47828bb0b26a8c11f0fbc5ad0
cl302 <cl686@uowmail.edu.au>
2026-03-06 13:24:13 +1100

    Create database.py

---
+++ backend_api/app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://postgres:123@localhost:5432/spherecare"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
