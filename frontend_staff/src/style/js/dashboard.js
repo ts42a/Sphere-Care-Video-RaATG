725e5551313dcfd1b8f72192c92ed52ab1eef214
yyyhhh424 <yong0715.yuhan@gmail.com>
2026-03-11 14:34:53 +1100

    Delete frontend_staff/src/style/js/dashboard.js

--- frontend_staff/src/style/js/dashboard.js
+++
@@ -1,9 +0,0 @@
-// GOOGLE OAUTH TOKEN HANDLER 
-(function handleOAuthToken() {
-  const params = new URLSearchParams(window.location.search);
-  const token  = params.get('token');
-  if (token) {
-    localStorage.setItem('access_token', token);
-    window.history.replaceState({}, document.title, window.location.pathname);
-  }
-})();
