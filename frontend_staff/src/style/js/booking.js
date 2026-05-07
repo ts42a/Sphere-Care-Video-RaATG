200700c79bab8d764906028dd52c355ea4078957
cl302 <cl686@uowmail.edu.au>
2026-05-07 15:49:54 +1000

    Update booking.js

--- frontend_staff/src/style/js/booking.js
+++ frontend_staff/src/style/js/booking.js
@@ -115,7 +115,7 @@ function renderMonth() {
   }
 }
 
-const HOURS = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+const HOURS = [7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 const DOWS  = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
 const HOUR_H = 52;
 
