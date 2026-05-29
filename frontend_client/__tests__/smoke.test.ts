7c9fbcb25a80063fb0d224bd9e5ef82042f7ca22
cl302 <cl686@uowmail.edu.au>
2026-05-29 12:01:38 +1000

    Add files via upload

---
+++ frontend_client/__tests__/smoke.test.ts
@@ -0,0 +1,9 @@
+describe("Frontend test setup", () => {
+  test("Jest can run tests", () => {
+    expect(true).toBe(true);
+  });
+
+  test("basic calculation works", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
